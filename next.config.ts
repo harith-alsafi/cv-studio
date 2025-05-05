4ee00dfa2d941e3e62d3412db0f6679235d24dea
Harith alsafi <harith.alsafi@gmail.com>
2025-05-05 12:50:31 +0100

    ne3lan fix number 10000

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'standalone'
+  output: "standalone",
+
+  // experimental: {
+  // Enables copying of files from the source directory to the output directory
+  outputFileTracingRoot: process.cwd(),
+  // Include the templates directory in the file tracing
+  outputFileTracingIncludes: {
+    "/": ["templates/**/*"],
+  },
+  // },
 };
 
 export default nextConfig;
