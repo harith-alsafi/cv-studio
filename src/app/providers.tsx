d2159f24c7e8e35fe4dc7ad3239b8109943d9da2
Harith Al-Safi <harith.al-safi@bt.com>
2025-05-21 11:28:48 +0100

    added clerk

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,14 @@
 'use client'
 
 import { ThemeProvider } from 'next-themes'
+import { ClerkProvider } from '@clerk/nextjs'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-    return <ThemeProvider attribute="class">{children}</ThemeProvider>
+    return (
+        <ClerkProvider>
+            <ThemeProvider attribute="class">
+                {children}
+            </ThemeProvider>
+        </ClerkProvider>
+    )
 }
